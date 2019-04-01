46b85821973f0636d191d9239335d36d0c8920f5
kec <keckchris@outlook.com>
2019-04-01 21:43:15 +0200

    reformat clean up code arrangement

--- efwservice/src/main/java/de/iso/apps/EfwserviceApp.java
+++ efwservice/src/main/java/de/iso/apps/EfwserviceApp.java
@@ -2,37 +2,23 @@ package de.iso.apps;
 
 import de.iso.apps.config.ApplicationProperties;
 import de.iso.apps.config.DefaultProfileUtil;
-import de.iso.apps.service.dto.MailChangingDTO;
 import io.github.jhipster.config.JHipsterConstants;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.ByteArrayDeserializer;
-import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import javax.annotation.PostConstruct;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 @EnableKafka() @SpringBootApplication
 @EnableConfigurationProperties({LiquibaseProperties.class, ApplicationProperties.class}) @EnableDiscoveryClient
@@ -41,8 +27,7 @@ public class EfwserviceApp {
     private static final Logger log = LoggerFactory.getLogger(EfwserviceApp.class);
     
     private final Environment env;
-    @Autowired private KafkaProperties kafkaProperties;
-    @Value("${tpd.topic-name}") private String topicName;
+    
     
     public EfwserviceApp(Environment env) {
         this.env = env;
@@ -121,70 +106,4 @@ public class EfwserviceApp {
         }
     }
     
-    // Producer configuration
-    // omitted...
-    
-    // Consumer configuration
-    
-    // If you only need one kind of deserialization, you only need to set the
-    // Consumer configuration properties. Uncomment this and remove all others below.
-    @Bean
-    public Map<String, Object> consumerConfigs() {
-        Map<String, Object> props = new HashMap<>(kafkaProperties.buildConsumerProperties());
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "tpd-loggers");
-        
-        return props;
-    }
-    
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, MailChangingDTO> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, MailChangingDTO> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory());
-        
-        return factory;
-    }
-    
-    @Bean
-    public ConsumerFactory<String, MailChangingDTO> consumerFactory() {
-        JsonDeserializer<MailChangingDTO> jsonDeserializer = new JsonDeserializer<>();
-        jsonDeserializer.addTrustedPackages("*");
-        return new DefaultKafkaConsumerFactory<>(kafkaProperties.buildConsumerProperties(),
-                                                 new StringDeserializer(),
-                                                 jsonDeserializer);
-    }
-    
-    // String Consumer Configuration
-    
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerStringContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(stringConsumerFactory());
-        
-        return factory;
-    }
-    
-    @Bean
-    public ConsumerFactory<String, String> stringConsumerFactory() {
-        return new DefaultKafkaConsumerFactory<>(kafkaProperties.buildConsumerProperties(),
-                                                 new StringDeserializer(),
-                                                 new StringDeserializer());
-    }
-    
-    // Byte Array Consumer Configuration
-    
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, byte[]> kafkaListenerByteArrayContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, byte[]> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(byteArrayConsumerFactory());
-        return factory;
-    }
-    
-    @Bean
-    public ConsumerFactory<String, byte[]> byteArrayConsumerFactory() {
-        return new DefaultKafkaConsumerFactory<>(kafkaProperties.buildConsumerProperties(),
-                                                 new StringDeserializer(),
-                                                 new ByteArrayDeserializer());
-    }
 }
